c51fe9f7f97e9f6981c43ee15326cfaefcb6c9ab
gbaldini <gbaldini@unibz.it>
2017-11-26 23:13:31 +0100

    Copyright and file post.js

---
+++ src/post.js
@@ -0,0 +1,33 @@
+document.write('<!DOCTYPE html>\
+<html lang="en">\
+<head>\
+  <meta charset="utf-8">\
+  <meta name="viewport" content="width=device-width, initial-scale=1">\
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">\
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>\
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>\
+  <link rel="stylesheet" type="text/css" href="style.css">\
+</head>\
+<body>\
+<div class="event">\
+  <div class="row">\
+    <div class="col-sm-2" >\
+    </div>\
+    <div class="col-sm-8" style="background-color:white;border: 1px solid green; ">\
+      <div class="panel-body row">\
+        <div class="col-sm-9">\
+        <div class="panel panel-default">\
+        <div class="panel-heading">Title </div>\
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.4</div>\
+        <button type="button" class="btn btn-success">Read more</button>\
+        </div>\
+        <div class="col-sm-3"><img src="slide2.jpg" width= "200px" ></div>\
+    </div>\
+    <div class="col-sm-2" >\
+    </div>\
+  </div>\
+</div>\
+\
+</div>\
+</html>\
+');
