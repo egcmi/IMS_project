1435219bfe6a8d0f8b82d63b4f2aaf501e49b547
Emanuela <dookie@live.it>
2018-01-12 17:14:00 +0100

    contact email

--- src/php/email_contact.php
+++ src/php/email_contact.php
@@ -11,7 +11,7 @@ require 'PHPMailer/src/SMTP.php';
 
 
 
-function send_email($subj, $email, $content){
+function send_email($subj, $email, $contenthtml, $contentsimple){
 
 	$fromem = 'uniscoutbz@gmail.com'; 
 	$fromps = 'CsBz201518';
@@ -32,14 +32,14 @@ function send_email($subj, $email, $content){
 	    )
 	);
 
-    $mail->setFrom($fromem, 'Uniscout Team');
+    $mail->setFrom($fromem, 'UniScout Team');
     $mail->addAddress($email);
 
     $mail->isHTML(true);
     $mail->Subject = $subj;
-	$mail->Body    = $content;
+	$mail->Body    = $contenthtml;
 
-	$mail->AltBody = $content;
+	$mail->AltBody = $contentsimple;
 
     $mail->send();
 }
@@ -50,12 +50,22 @@ if(isset($_REQUEST)){
 	$email=$_POST['email'];
 	$comment=$_POST['comments'];
 
-	$body = "Hi {$name}! Thank you for contacting us. We are going to get back to you within a couple of days.";
+	$bodyhtml = "
+	Hi {$name}!<br>
+	Thank you for contacting us. We will get back to you as soon as possible.<br>
+	Here is what you wrote:<br>
+	<p><i>$comment</i><p>
+	If you wish to add more information, simply reply to this email.<br>
+	Cheers!<br>
+	Your UniScout Team
+	";
 
-	try{
-		send_email("You received an email from: {$name}, with the address: {$email}", "uniscoutbz@gmail.com", $comment);
-		send_email("Automatically generated response", $email, $body);
-	    echo 'Message has been sent';
+	$bodysimple = "Hi {$name}! Thank you for contacting us. We will get back to you as soon as possible. Here is what you wrote: \"{$comment}\" If you wish to add more information, simply reply to this email. Cheers! Your UniScout Team";
+
+try{
+		send_email("Message from: {$name} <{$email}>", "uniscoutbz@gmail.com", $comment, $comment);
+		send_email("Your message to UniScout", $email, $bodyhtml, $bodysimple);
+	    echo 'Thank you! We have received your message.';
 	}catch(Exception $e){
 		echo 'Message could not be sent. Please try again.';
 	    //echo 'Mailer Error: ' . $mail->ErrorInfo;
