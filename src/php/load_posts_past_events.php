95550ee54e919c3329f830a84b40ae6c74d1e8cc
mikel_grabocka <mikelgrabocka@hotmail.com>
2018-01-18 09:03:52 +0100

    db mod

--- src/php/load_posts_past_events.php
+++ src/php/load_posts_past_events.php
@@ -12,7 +12,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "SELECT id_event, title, date_time, description FROM articles ORDER BY date_time ASC;";
+$sql = "SELECT id_event, title, date_time, description FROM articles ORDER BY date_time DESC;";
 $result = $conn->query($sql);
 $rows = array();
 $id = -1;
